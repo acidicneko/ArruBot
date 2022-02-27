d6d0b05e6a6e743f4e92bcc38f3af399f56d39a3
clawbhaiya <ayush7788899@gmail.com>
2022-02-27 16:24:14 +0530

    make BOT_TOKEN Read from variables

--- src/main/java/arru/Arru.java
+++ src/main/java/arru/Arru.java
@@ -6,15 +6,14 @@ import arru.info.Constants;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import io.github.cdimascio.dotenv.Dotenv;
 
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 
 class Arru extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
-        Constants.botPrefix = Dotenv.load().get("BOT_PREFIX");
-        JDABuilder.createDefault(Dotenv.load().get("BOT_TOKEN"))
+        Constants.botPrefix = System.getenv("BOT_PREFIX");//Dotenv.load().get("BOT_PREFIX");
+        JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
                     .setActivity(Activity.playing("with your life"))
                     .enableIntents(GatewayIntent.GUILD_VOICE_STATES)
                     .enableIntents(GatewayIntent.GUILD_MEMBERS)

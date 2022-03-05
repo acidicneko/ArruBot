ae163bd51b03a01217d39a7feae6d99e69f62d9b
clawbhaiya <ayush7788899@gmail.com>
2022-03-05 08:42:35 +0530

    refactor version embed

--- src/main/java/arru/cmdlets/Version.java
+++ src/main/java/arru/cmdlets/Version.java
@@ -1,6 +1,7 @@
 package arru.cmdlets;
 
 import java.awt.Color;
+import java.time.Instant;
 import java.util.List;
 
 import arru.info.Constants;
@@ -13,11 +14,17 @@ public class Version implements Cmd {
     @Override
     public void execute(List<String> argv, MessageReceivedEvent event) {
         EmbedBuilder eb = new EmbedBuilder();
+        eb.setAuthor(event.getJDA().getSelfUser().getName() +
+                        "#" + event.getJDA().getSelfUser().getDiscriminator(), null,
+                        event.getJDA().getSelfUser().getAvatarUrl());
+        eb.setThumbnail("https://raw.githubusercontent.com/clawbhaiya/ArruBot/main/images/arru-img.jpg");
         eb.setTitle(":cat: Arru the cat");
         eb.setColor(Color.BLUE);
+        eb.setDescription("Made with :brown_heart: by Clawbhaiya.");
         eb.addField("Version", Constants.version, false);
-        eb.addBlankField(false);
         eb.addField("Written in", ":coffee: Java (JDK17)", false);
+        eb.setFooter("ArruChan", event.getJDA().getSelfUser().getAvatarUrl());
+        eb.setTimestamp(Instant.now());
         event.getChannel().sendMessageEmbeds(eb.build()).queue();
     }
 

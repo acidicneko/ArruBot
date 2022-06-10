02e4ea888b7f056a2a3e86a3e81631ddea59787c
clawbhaiya <ayush7788899@gmail.com>
2022-06-10 11:53:04 +0530

    change activity status

--- src/main/java/arru/Arru.java
+++ src/main/java/arru/Arru.java
@@ -14,7 +14,7 @@ class Arru extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         Constants.botPrefix = System.getenv("BOT_PREFIX");//Dotenv.load().get("BOT_PREFIX");
         JDABuilder.createDefault(System.getenv("BOT_TOKEN"))
-                    .setActivity(Activity.playing("with your life"))
+                    .setActivity(Activity.watching("your mom"))
                     .enableIntents(GatewayIntent.GUILD_VOICE_STATES)
                     .enableIntents(GatewayIntent.GUILD_MEMBERS)
                     .addEventListeners(new Listener())
